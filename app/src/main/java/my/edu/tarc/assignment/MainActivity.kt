b60a713ed50cc01581c7258c1267b46b9fe9901b
cheokjw <cheok1121@gmail.com>
2022-12-10 11:39:08 +0800

    Initial Commit

---
+++ app/src/main/java/my/edu/tarc/assignment/MainActivity.kt
@@ -0,0 +1,45 @@
+package my.edu.tarc.assignment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import my.edu.tarc.assignment.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        // This is what users see when they first use our app (in this case: CheckIn Fragment will be shown first)
+        replaceFragment(CheckIn())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.checkin -> replaceFragment(CheckIn())
+                R.id.quiz -> replaceFragment(Quiz())
+                R.id.news -> replaceFragment(News())
+                R.id.tree -> replaceFragment(Tree())
+                R.id.profile -> replaceFragment(Profile())
+
+            }
+
+            true
+        }
+    }
+
+
+    // Allowing fragment to replace the main Body
+    private fun replaceFragment(fragment : Fragment) {
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        // Selecting which part of the UI should be replaced by the fragment
+        // in this case its the frameLayout in activity_main.xml
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.commit()
+    }
+}
