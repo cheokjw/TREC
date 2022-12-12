29b120fa9a5b4a1775bd887fdf1bdb6b80841c5d
xyan <62557207+bryanlsx@users.noreply.github.com>
2022-12-12 17:09:51 +0800

    Added Design for Check In Page

--- app/src/main/java/my/edu/tarc/assignment/CheckIn.kt
+++ app/src/main/java/my/edu/tarc/assignment/CheckIn.kt
@@ -4,12 +4,11 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.OnClickListener
 import android.view.ViewGroup
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
+import my.edu.tarc.assignment.databinding.FragmentCheckInBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -17,43 +16,27 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class CheckIn : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var bindingCheckIn: FragmentCheckInBinding
+    private lateinit var builder: AlertDialog.Builder
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_check_in, container, false)
+        bindingCheckIn = FragmentCheckInBinding.inflate(inflater)
+
+        bindingCheckIn.buttonCheckIn.setOnClickListener {
+            bindingCheckIn.notCheckedInStatus.setImageResource(R.drawable.checkedin)
+        }
+        return bindingCheckIn.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CheckIn.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CheckIn().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+    //TODO: implement pop up scrollview for rewards history
+
+
 }
