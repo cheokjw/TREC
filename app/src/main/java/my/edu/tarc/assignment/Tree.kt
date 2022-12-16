8c16ae394189a05a0c96052e2a0887198567b98e
wclow <wclow0420@gmail.com>
2022-12-17 00:05:17 +0800

    potion shop and tree grow page all function done

--- app/src/main/java/my/edu/tarc/assignment/Tree.kt
+++ app/src/main/java/my/edu/tarc/assignment/Tree.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.FragmentTransaction
 import my.edu.tarc.assignment.databinding.FragmentPotionshopBinding
 import my.edu.tarc.assignment.databinding.FragmentTreeBinding
@@ -23,6 +24,10 @@ private const val ARG_PARAM2 = "param2"
 class Tree : Fragment() {
     private lateinit var bindingTree:FragmentTreeBinding
     private lateinit var bindingPotion: FragmentPotionshopBinding
+    var sprayqt = 5
+    var greenqt = 5
+    var goldqt = 5
+    var barprocess = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,11 +45,175 @@ class Tree : Fragment() {
         bindingTree.treeShopButton.setOnClickListener {
             replaceFragment(treeshop())
         }
+
+        bindingTree.itemonebutton.setOnClickListener{
+            if(sprayqt>=1){
+                if(barprocess >=20) {
+                    barprocess = 0
+                    Toast.makeText(activity, "Successfully Received 1 TREE COIN!\n Spray Balance: "+ sprayqt , Toast.LENGTH_SHORT).show()
+                }else{
+                    sprayqt -= 1
+                    barprocess += 1
+                    Toast.makeText(activity, "Successfully spent 1 Spray!\n Spray Balance: "+ sprayqt , Toast.LENGTH_SHORT).show()
+
+                }
+                replaceTree(barprocess)
+                replacePhoto(barprocess)
+                }else if(barprocess >=20) {
+                barprocess = 0
+                Toast.makeText(activity, "Successfully Received 1 TREE COIN!\n Spray Balance: " + sprayqt, Toast.LENGTH_SHORT).show()
+                replaceTree(barprocess)
+                replacePhoto(barprocess)
+                }else
+                Toast.makeText(activity, "Insufficient Spray Potion!\n Earn more to PLAY!!!", Toast.LENGTH_SHORT).show()
+        }
+
+
+        bindingTree.itemtwobutton.setOnClickListener{
+            if(greenqt>=1){
+                if(barprocess >=20) {
+                    barprocess = 0
+                    Toast.makeText(activity, "Successfully Received 1 TREE COIN!\n Green Pot Balance: "+ greenqt , Toast.LENGTH_SHORT).show()
+                    if(barprocess == 0){
+                        replacePhoto(barprocess)
+                    }
+                }else{
+                    greenqt -= 1
+                    barprocess += 2
+                    Toast.makeText(activity, "Successfully spent 1 Green Pot!\n Green Pot Balance: "+ greenqt , Toast.LENGTH_SHORT).show()
+                }
+                replaceTree(barprocess)
+                replacePhoto(barprocess)
+                }else if(barprocess >=20) {
+                barprocess = 0
+                Toast.makeText(activity, "Successfully Received 1 TREE COIN!\n Green Pot Balance: " + greenqt, Toast.LENGTH_SHORT).show()
+                replaceTree(barprocess)
+                replacePhoto(barprocess)
+                }else
+                Toast.makeText(activity, "Insufficient Green Pot!\n Earn more to PLAY!!!", Toast.LENGTH_SHORT).show()
+        }
+
+        bindingTree.itemthreebutton.setOnClickListener{
+            if(goldqt>=1){
+                if(barprocess >= 20) {
+                    barprocess = 0
+                    Toast.makeText(activity, "Successfully Received 1 TREE COIN!\n Gold Pot Balance: "+ goldqt , Toast.LENGTH_SHORT).show()
+                    if(barprocess == 0){
+                        replacePhoto(barprocess)
+                    }
+                }else{
+                    goldqt -= 1
+                    barprocess += 4
+                    Toast.makeText(activity, "Successfully spent 1 Gold Pot!\n Gold Pot Balance: "+ goldqt , Toast.LENGTH_SHORT).show()
+                }
+                replaceTree(barprocess)
+                replacePhoto(barprocess)
+                }else if(barprocess >=20) {
+                barprocess = 0
+                Toast.makeText(activity, "Successfully Received 1 TREE COIN!\n Gold Pot Balance: " + goldqt, Toast.LENGTH_SHORT).show()
+                replaceTree(barprocess)
+                replacePhoto(barprocess)
+                }else
+                Toast.makeText(activity, "Insufficient Gold Pot!\n Earn more to PLAY!!!", Toast.LENGTH_SHORT).show()
+        }
+
+
         return bindingTree.root
 
 //        return inflater.inflate(R.layout.fragment_tree, container, false)
 
 
+    }
+
+    private fun replaceTree(barprocess: Number){
+        if(barprocess == 0)
+            bindingTree.treeone.setImageResource(R.drawable.treeone)
+        else if(barprocess == 1)
+            bindingTree.treeone.setImageResource(R.drawable.treeone)
+        else if(barprocess == 2)
+            bindingTree.treeone.setImageResource(R.drawable.treeone)
+        else if(barprocess == 3)
+            bindingTree.treeone.setImageResource(R.drawable.treeone)
+        else if(barprocess == 4)
+            bindingTree.treeone.setImageResource(R.drawable.treeone)
+        else if(barprocess == 5)
+            bindingTree.treeone.setImageResource(R.drawable.treeone)
+        else if(barprocess == 6)
+            bindingTree.treeone.setImageResource(R.drawable.treeone)
+        else if(barprocess == 7)
+            bindingTree.treeone.setImageResource(R.drawable.treeone)
+        else if(barprocess == 8)
+            bindingTree.treeone.setImageResource(R.drawable.treeone)
+        else if(barprocess == 9)
+            bindingTree.treeone.setImageResource(R.drawable.treeone)
+        else if(barprocess == 10)
+            bindingTree.treeone.setImageResource(R.drawable.treetwo)
+        else if(barprocess == 11)
+            bindingTree.treeone.setImageResource(R.drawable.treetwo)
+        else if(barprocess == 12)
+            bindingTree.treeone.setImageResource(R.drawable.treetwo)
+        else if(barprocess == 13)
+            bindingTree.treeone.setImageResource(R.drawable.treetwo)
+        else if(barprocess == 14)
+            bindingTree.treeone.setImageResource(R.drawable.treetwo)
+        else if(barprocess == 15)
+            bindingTree.treeone.setImageResource(R.drawable.treethree)
+        else if(barprocess == 16)
+            bindingTree.treeone.setImageResource(R.drawable.treethree)
+        else if(barprocess == 17)
+            bindingTree.treeone.setImageResource(R.drawable.treethree)
+        else if(barprocess == 18)
+            bindingTree.treeone.setImageResource(R.drawable.treethree)
+        else if(barprocess == 19)
+            bindingTree.treeone.setImageResource(R.drawable.treethree)
+        else if(barprocess == 20)
+            bindingTree.treeone.setImageResource(R.drawable.treefour)
+        else
+            bindingTree.treeone.setImageResource(R.drawable.treefour)
+    }
+    private fun replacePhoto(barprocess: Number){
+        if(barprocess == 0)
+            bindingTree.finishbar.setImageResource(R.drawable.emptybar)
+        else if(barprocess == 1)
+            bindingTree.finishbar.setImageResource(R.drawable.onebar)
+        else if(barprocess == 2)
+            bindingTree.finishbar.setImageResource(R.drawable.twobar)
+        else if(barprocess == 3)
+            bindingTree.finishbar.setImageResource(R.drawable.threebar)
+        else if(barprocess == 4)
+            bindingTree.finishbar.setImageResource(R.drawable.fourbar)
+        else if(barprocess == 5)
+            bindingTree.finishbar.setImageResource(R.drawable.fivebar)
+        else if(barprocess == 6)
+            bindingTree.finishbar.setImageResource(R.drawable.sixbar)
+        else if(barprocess == 7)
+            bindingTree.finishbar.setImageResource(R.drawable.sevenbar)
+        else if(barprocess == 8)
+            bindingTree.finishbar.setImageResource(R.drawable.eightbar)
+        else if(barprocess == 9)
+            bindingTree.finishbar.setImageResource(R.drawable.ninebar)
+        else if(barprocess == 10)
+            bindingTree.finishbar.setImageResource(R.drawable.tenbar)
+        else if(barprocess == 11)
+            bindingTree.finishbar.setImageResource(R.drawable.elevenbar)
+        else if(barprocess == 12)
+            bindingTree.finishbar.setImageResource(R.drawable.twelvebar)
+        else if(barprocess == 13)
+            bindingTree.finishbar.setImageResource(R.drawable.thirdbar)
+        else if(barprocess == 14)
+            bindingTree.finishbar.setImageResource(R.drawable.fourthbar)
+        else if(barprocess == 15)
+            bindingTree.finishbar.setImageResource(R.drawable.fivethbar)
+        else if(barprocess == 16)
+            bindingTree.finishbar.setImageResource(R.drawable.sixthbar)
+        else if(barprocess == 17)
+            bindingTree.finishbar.setImageResource(R.drawable.seventhbar)
+        else if(barprocess == 18)
+            bindingTree.finishbar.setImageResource(R.drawable.eightthbar)
+        else if(barprocess == 19)
+            bindingTree.finishbar.setImageResource(R.drawable.ninethbar)
+        else
+            bindingTree.finishbar.setImageResource(R.drawable.tenthbar)
     }
     //     Allowing fragment to replace the main Body
     private fun replaceFragment(fragment : Fragment){
