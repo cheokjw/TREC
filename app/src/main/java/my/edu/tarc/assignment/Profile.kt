3053d34732a00f65d3af5c5a10d526623bd6e734
EHii2001 <118877821+EHii2001@users.noreply.github.com>
2022-12-17 12:16:33 +0800

    - design of avatar and forgt pw

--- app/src/main/java/my/edu/tarc/assignment/Profile.kt
+++ app/src/main/java/my/edu/tarc/assignment/Profile.kt
@@ -5,9 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import my.edu.tarc.assignment.databinding.FragmentProfileBinding
 
-class Profile : Fragment() {
+class Profile : Fragment(), View.OnClickListener {
 
+    private lateinit var bindingProfile:FragmentProfileBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,9 +20,29 @@ class Profile : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        bindingProfile = FragmentProfileBinding.inflate(layoutInflater)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        return bindingProfile.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        bindingProfile.cvAvatar.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when(v.id){
+            R.id.cvAvatar -> {
+                val showPopUp = Avatar()
+                showPopUp.show((activity as AppCompatActivity).supportFragmentManager, "showPopUp")
+            }
+        }
+
     }
 
 
 }
+
+
