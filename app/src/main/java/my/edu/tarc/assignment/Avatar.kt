a2a0390ddd39c7d0c3ae225f74fd5b0deb16d833
EHii2001 <118877821+EHii2001@users.noreply.github.com>
2022-12-25 19:09:28 +0800

    -add select avatar function

--- app/src/main/java/my/edu/tarc/assignment/Avatar.kt
+++ app/src/main/java/my/edu/tarc/assignment/Avatar.kt
@@ -1,16 +1,18 @@
 package my.edu.tarc.assignment
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.*
+import androidx.core.graphics.alpha
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import my.edu.tarc.assignment.databinding.FragmentAvatarBinding
 
 class Avatar : DialogFragment() {
 
     private lateinit var bindingAvatar: FragmentAvatarBinding
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,13 +22,74 @@ class Avatar : DialogFragment() {
         return bindingAvatar.root
     }
 
+
+    @SuppressLint("ClickableViewAccessibility")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        //finger touch on avatar
+        bindingAvatar.imageViewAvatar1.setOnClickListener {
+            bindingAvatar.imageViewAvatar1.alpha = 1F
+            bindingAvatar.imageViewAvatar2.alpha = 0.5F
+            bindingAvatar.imageViewAvatar3.alpha = 0.5F
+            bindingAvatar.imageViewAvatar4.alpha = 0.5F
+            bindingAvatar.imageViewAvatar5.alpha = 0.5F
+            bindingAvatar.imageViewAvatar6.alpha = 0.5F
+        }
+
+        bindingAvatar.imageViewAvatar2.setOnClickListener {
+            bindingAvatar.imageViewAvatar1.alpha = 0.5F
+            bindingAvatar.imageViewAvatar2.alpha = 1F
+            bindingAvatar.imageViewAvatar3.alpha = 0.5F
+            bindingAvatar.imageViewAvatar4.alpha = 0.5F
+            bindingAvatar.imageViewAvatar5.alpha = 0.5F
+            bindingAvatar.imageViewAvatar6.alpha = 0.5F
+        }
+
+        bindingAvatar.imageViewAvatar3.setOnClickListener {
+            bindingAvatar.imageViewAvatar1.alpha = 0.5F
+            bindingAvatar.imageViewAvatar2.alpha = 0.5F
+            bindingAvatar.imageViewAvatar3.alpha = 1F
+            bindingAvatar.imageViewAvatar4.alpha = 0.5F
+            bindingAvatar.imageViewAvatar5.alpha = 0.5F
+            bindingAvatar.imageViewAvatar6.alpha = 0.5F
+        }
+
+        bindingAvatar.imageViewAvatar4.setOnClickListener {
+            bindingAvatar.imageViewAvatar1.alpha = 0.5F
+            bindingAvatar.imageViewAvatar2.alpha = 0.5F
+            bindingAvatar.imageViewAvatar3.alpha = 0.5F
+            bindingAvatar.imageViewAvatar4.alpha = 1F
+            bindingAvatar.imageViewAvatar5.alpha = 0.5F
+            bindingAvatar.imageViewAvatar6.alpha = 0.5F
+        }
+
+        bindingAvatar.imageViewAvatar5.setOnClickListener {
+            bindingAvatar.imageViewAvatar1.alpha = 0.5F
+            bindingAvatar.imageViewAvatar2.alpha = 0.5F
+            bindingAvatar.imageViewAvatar3.alpha = 0.5F
+            bindingAvatar.imageViewAvatar4.alpha = 0.5F
+            bindingAvatar.imageViewAvatar5.alpha = 1F
+            bindingAvatar.imageViewAvatar6.alpha = 0.5F
+        }
+
+        bindingAvatar.imageViewAvatar6.setOnClickListener {
+            bindingAvatar.imageViewAvatar1.alpha = 0.5F
+            bindingAvatar.imageViewAvatar2.alpha = 0.5F
+            bindingAvatar.imageViewAvatar3.alpha = 0.5F
+            bindingAvatar.imageViewAvatar4.alpha = 0.5F
+            bindingAvatar.imageViewAvatar5.alpha = 0.5F
+            bindingAvatar.imageViewAvatar6.alpha = 1F
+        }
+
 
         bindingAvatar.imageButtonQuit.setOnClickListener {
             dismiss()
         }
 
+
+
     }
+
 
 }
