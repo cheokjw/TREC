1ca9ad6b79858d8d47c0af13f9bb2e59b4446906
cheokjw <cheok1121@gmail.com>
2022-12-30 23:24:39 +0800

    - completed news section

--- app/src/main/java/my/edu/tarc/assignment/News.kt
+++ app/src/main/java/my/edu/tarc/assignment/News.kt
@@ -1,14 +1,20 @@
 package my.edu.tarc.assignment
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.FragmentActivity
+import my.edu.tarc.assignment.databinding.FragmentNewsBinding
 
 
 class News : Fragment() {
 
+    private lateinit var bindingNews: FragmentNewsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,10 +25,40 @@ class News : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_news, container, false)
+        bindingNews = FragmentNewsBinding.inflate(layoutInflater)
+        return bindingNews.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val activity: FragmentActivity? = activity
+
+        bindingNews.cvWho.setOnClickListener{
+            Toast.makeText(activity, "Guiding you to WHO website", Toast.LENGTH_LONG).show()
+            val url = "https://www.who.int"
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(url)
+            startActivity(intent)
+        }
+
+        bindingNews.cvGec.setOnClickListener{
+            Toast.makeText(activity, "Guiding you to GEC website", Toast.LENGTH_LONG).show()
+            val url = "https://www.gec.org.my"
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(url)
+            startActivity(intent)
+        }
+
+        bindingNews.cvCr.setOnClickListener{
+            Toast.makeText(activity, "Guiding you to GEC website", Toast.LENGTH_LONG).show()
+            val url = "https://www.charityright.my"
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(url)
+            startActivity(intent)
+        }
+
+
+    }
 
 
 }
