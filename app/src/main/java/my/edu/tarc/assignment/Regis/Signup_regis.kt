ddb15c77014ba01a9ae1302e86f5e234c211df37
xyan <62557207+bryanlsx@users.noreply.github.com>
2022-12-31 12:27:49 +0800

    - minor update on Signup_regis.kt and Signup.kt

--- app/src/main/java/my/edu/tarc/assignment/Regis/Signup_regis.kt
+++ app/src/main/java/my/edu/tarc/assignment/Regis/Signup_regis.kt
@@ -1,5 +1,5 @@
 package my.edu.tarc.assignment.Regis
 
 class Signup_regis (var username:String, var fullname:String, var phone: String, var address: String, var email: String, var pass: String,
-                    var idSign:String, var treeCoin: Int, var gameCoin: Int){
+                    var idSign:String, var treeCoin: Int, var gameCoin: Int, var checkin:Int){
 }
