906568402de69e1c908bd3c2deef8a3dccf8096b
cheokjw <cheok1121@gmail.com>
2023-01-01 15:39:23 +0800

    - Added checkin update checkin count and gamecoin balance function

--- app/src/main/java/my/edu/tarc/assignment/CheckIn.kt
+++ app/src/main/java/my/edu/tarc/assignment/CheckIn.kt
@@ -2,6 +2,7 @@ package my.edu.tarc.assignment
 
 import android.content.Context
 import android.os.Bundle
+import android.service.controls.ControlsProviderService.TAG
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -11,153 +12,205 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.FragmentActivity
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.android.material.tabs.TabLayout.TabGravity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthException
+import com.google.firebase.database.*
+import com.google.firebase.database.core.Tag
+import my.edu.tarc.assignment.Regis.Signup_regis
 import my.edu.tarc.assignment.databinding.FragmentCheckInBinding
 
-
-class CheckIn: Fragment() {
+class CheckIn : Fragment() {
     private lateinit var bindingCheckIn: FragmentCheckInBinding
 
-    lateinit var counterTextView: TextView
-    lateinit var builder : AlertDialog.Builder
+    private lateinit var counterTextView: TextView
+    private lateinit var builder : AlertDialog.Builder
 
     lateinit var database: FirebaseDatabase
     lateinit var databaseReference: DatabaseReference
 
-    private var checkInCount = 0
     private var counter = 0
-    private var user = ""
-
-    var gameCoinBal = 0
+    var gameCoin = 0
     val handler = android.os.Handler()
-
+    private var checkin = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        database = FirebaseDatabase.getInstance()
-        databaseReference = database.getReference().child("user")
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        bindingCheckIn = FragmentCheckInBinding.inflate(layoutInflater)
+        bindingCheckIn = FragmentCheckInBinding.inflate(inflater)
         return bindingCheckIn.root
     }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val activity: FragmentActivity?= activity
+
+        // Get who is the current user
+        val mainActivity: MainActivity = getActivity() as MainActivity
+        val sessionUser = mainActivity.getSessionUser()
+        database = FirebaseDatabase.getInstance()
+        databaseReference = database.getReference("user")
+
+        //Starting Point of the Check In Bar
         bindingCheckIn.progressBarCheckIn.progress = 0
 
         //Show Balance Constantly
+        counterTextView = bindingCheckIn.textViewRegBalance
         handler.post(updateCounter)
 
         //Check In Button
         bindingCheckIn.buttonCheckIn.setOnClickListener {
             bindingCheckIn.notCheckedInStatus.setImageResource(R.drawable.checkedin)
 
-//        if(checkInCount<1) {
-//            when (counter) {
-//                0 -> {
-//                    //Day 1 CheckIn
-//                    bindingCheckIn.progressBarCheckIn.progress = 0
-//                    bindingCheckIn.imageViewDay1.setImageResource((R.drawable.checked_in_progress))
-//                    Toast.makeText(activity, "5 Coins Added!$gameCoinBalance", Toast.LENGTH_SHORT)
-//                        .show()
-//                    counter++
-//                    checkInCount++
-//                    gameCoinBalance += 5
-//
-//                }
-//                1 -> {
-//                    //Day 2 CheckIn
-//                    //If Statement to Prevent User from checking in twice
-//                    bindingCheckIn.progressBarCheckIn.progress =
-//                        (bindingCheckIn.progressBarCheckIn.progress + 15) % 100
-//                    bindingCheckIn.imageViewDay2.setImageResource((R.drawable.checked_in_progress))
-//                    gameCoinBalance += 5
-//                    Toast.makeText(activity, "5 Coins Added!$gameCoinBalance", Toast.LENGTH_SHORT)
-//                        .show()
-//                    counter++
-//                    checkInCount++
-//
-//                }
-//                2 -> {
-//                    //Day 3 CheckIn
-//                    bindingCheckIn.progressBarCheckIn.progress =
-//                        (bindingCheckIn.progressBarCheckIn.progress + 15) % 100
-//                    bindingCheckIn.imageViewDay3.setImageResource((R.drawable.checked_in_progress))
-//                    gameCoinBalance += 5
-//                    Toast.makeText(activity, "5 Coins Added!$gameCoinBalance", Toast.LENGTH_SHORT)
-//                        .show()
-//                    counter++
-//                }
-//                3 -> {
-//                    //Day 4 CheckIn
-//                    bindingCheckIn.progressBarCheckIn.progress =
-//                        (bindingCheckIn.progressBarCheckIn.progress + 15) % 100
-//                    bindingCheckIn.imageViewDay4.setImageResource((R.drawable.checked_in_progress))
-//                    gameCoinBalance += 5
-//                    Toast.makeText(activity, "5 Coins Added!$gameCoinBalance", Toast.LENGTH_SHORT)
-//                        .show()
-//                    counter++
-//                }
-//                4 -> {
-//                    //Day 5 CheckIn
-//                    bindingCheckIn.progressBarCheckIn.progress =
-//                        (bindingCheckIn.progressBarCheckIn.progress + 15) % 100
-//                    bindingCheckIn.imageViewDay5.setImageResource((R.drawable.checked_in_progress))
-//                    gameCoinBalance += 5
-//                    Toast.makeText(activity, "5 Coins Added!$gameCoinBalance", Toast.LENGTH_SHORT)
-//                        .show()
-//                    counter++
-//                }
-//                5 -> {
-//                    //Day 6 CheckIn
-//                    bindingCheckIn.progressBarCheckIn.progress =
-//                        (bindingCheckIn.progressBarCheckIn.progress + 15) % 100
-//                    bindingCheckIn.imageViewDay6.setImageResource((R.drawable.checked_in_progress))
-//                    gameCoinBalance += 5
-//                    Toast.makeText(activity, "5 Coins Added!$gameCoinBalance", Toast.LENGTH_SHORT)
-//                        .show()
-//                    counter++
-//                }
-//                6 -> {
-//                    //Day 7 CheckIn
-//                    bindingCheckIn.progressBarCheckIn.progress = 100
-//                    bindingCheckIn.imageViewDay7.setImageResource((R.drawable.checked_in_progress))
-//                    gameCoinBalance += 25
-//                    Toast.makeText(activity, "25 Coins Added!$gameCoinBalance", Toast.LENGTH_SHORT)
-//                        .show()
-//                    counter = 0
-//
-//                    //Show Pop To Notify User
-//                    builder = AlertDialog.Builder(activity!!)
-//                    builder.setTitle("Congratulations!")
-//                        .setMessage("You have checked in for 7 days")
-//                        .setCancelable(true)
-//                        .setPositiveButton("Ok") { dialogInterface, it ->
-//                            bindingCheckIn.progressBarCheckIn.progress = 0
-//                            bindingCheckIn.imageViewDay1.setImageResource((R.drawable.check_in_progress))
-//                            bindingCheckIn.imageViewDay2.setImageResource((R.drawable.check_in_progress))
-//                            bindingCheckIn.imageViewDay3.setImageResource((R.drawable.check_in_progress))
-//                            bindingCheckIn.imageViewDay4.setImageResource((R.drawable.check_in_progress))
-//                            bindingCheckIn.imageViewDay5.setImageResource((R.drawable.check_in_progress))
-//                            bindingCheckIn.imageViewDay6.setImageResource((R.drawable.check_in_progress))
-//                            bindingCheckIn.imageViewDay7.setImageResource((R.drawable.check_in_progress))
-//                            dialogInterface.cancel()
-//                        }
-//                        .show()
-//                }
-//            }
-//        }else{
-//            Toast.makeText(activity, "Already Checked In Today!\nPlease Try Again Tomorrow!", Toast.LENGTH_SHORT).show()
-//        }
+            databaseReference.child(sessionUser).get().addOnSuccessListener {
+
+                if (it.exists()){
+
+                    var dbCheckIn = it.child(sessionUser).child("checkin").value
+                    var dbGameCoin = it.child(sessionUser).child("gameCoin").value
+                    var checkin : Int
+
+                    checkin = if (dbCheckIn == null){
+                        0
+                    }else {
+                        dbCheckIn as Int
+                    }
+
+                    gameCoin = if (dbGameCoin == null){
+                        0
+                    }else {
+                        dbGameCoin as Int
+                    }
+
+                    if(checkin<1) {
+                        when (counter) {
+                            0 -> {
+                                //Day 1 CheckIn
+                                bindingCheckIn.progressBarCheckIn.progress = 0
+                                bindingCheckIn.imageViewDay1.setImageResource((R.drawable.checked_in_progress))
+                                gameCoin += 5
+                                Toast.makeText(activity, "5 Coins Added!$gameCoin", Toast.LENGTH_SHORT).show()
+                                counter++
+                                checkin++
+
+                                databaseReference.child(sessionUser).child("checkin").setValue(checkin)
+                                databaseReference.child(sessionUser).child("gameCoin").setValue(gameCoin)
+                            }
+                            1 -> {
+                                //Day 2 CheckIn
+                                //If Statement to Prevent User from checking in twice
+                                bindingCheckIn.progressBarCheckIn.progress = (bindingCheckIn.progressBarCheckIn.progress + 15) % 100
+                                bindingCheckIn.imageViewDay2.setImageResource((R.drawable.checked_in_progress))
+                                gameCoin += 5
+                                Toast.makeText(activity, "5 Coins Added!$gameCoin", Toast.LENGTH_SHORT)
+                                    .show()
+                                counter++
+                                checkin++
+                                databaseReference.child(sessionUser).child("checkin").setValue(checkin)
+                                databaseReference.child(sessionUser).child("gameCoin").setValue(gameCoin)
+
+                            }
+                            2 -> {
+                                //Day 3 CheckIn
+                                bindingCheckIn.progressBarCheckIn.progress = (bindingCheckIn.progressBarCheckIn.progress + 15) % 100
+                                bindingCheckIn.imageViewDay2.setImageResource((R.drawable.checked_in_progress))
+                                gameCoin += 5
+                                Toast.makeText(activity, "5 Coins Added!$gameCoin", Toast.LENGTH_SHORT)
+                                    .show()
+                                counter++
+                                checkin++
+                                databaseReference.child(sessionUser).child("checkin").setValue(checkin)
+                                databaseReference.child(sessionUser).child("gameCoin").setValue(gameCoin)
+                            }
+                            3 -> {
+                                //Day 4 CheckIn
+                                bindingCheckIn.progressBarCheckIn.progress = (bindingCheckIn.progressBarCheckIn.progress + 15) % 100
+                                bindingCheckIn.imageViewDay2.setImageResource((R.drawable.checked_in_progress))
+                                gameCoin += 5
+                                Toast.makeText(activity, "5 Coins Added!$gameCoin", Toast.LENGTH_SHORT)
+                                    .show()
+                                counter++
+                                checkin++
+                                databaseReference.child(sessionUser).child("checkin").setValue(checkin)
+                                databaseReference.child(sessionUser).child("gameCoin").setValue(gameCoin)
+                            }
+                            4 -> {
+                                //Day 5 CheckIn
+                                bindingCheckIn.progressBarCheckIn.progress = (bindingCheckIn.progressBarCheckIn.progress + 15) % 100
+                                bindingCheckIn.imageViewDay2.setImageResource((R.drawable.checked_in_progress))
+                                gameCoin += 5
+                                Toast.makeText(activity, "5 Coins Added!$gameCoin", Toast.LENGTH_SHORT)
+                                    .show()
+                                counter++
+                                checkin++
+                                databaseReference.child(sessionUser).child("checkin").setValue(checkin)
+                                databaseReference.child(sessionUser).child("gameCoin").setValue(gameCoin)
+                            }
+                            5 -> {
+                                //Day 6 CheckIn
+                                bindingCheckIn.progressBarCheckIn.progress = (bindingCheckIn.progressBarCheckIn.progress + 15) % 100
+                                bindingCheckIn.imageViewDay2.setImageResource((R.drawable.checked_in_progress))
+                                gameCoin += 5
+                                Toast.makeText(activity, "5 Coins Added!$gameCoin", Toast.LENGTH_SHORT)
+                                    .show()
+                                counter++
+                                checkin++
+                                databaseReference.child(sessionUser).child("checkin").setValue(checkin)
+                                databaseReference.child(sessionUser).child("gameCoin").setValue(gameCoin)
+                            }
+                            6 -> {
+                                //Day 7 CheckIn
+                                bindingCheckIn.progressBarCheckIn.progress = (bindingCheckIn.progressBarCheckIn.progress + 15) % 100
+                                bindingCheckIn.imageViewDay2.setImageResource((R.drawable.checked_in_progress))
+                                gameCoin += 25
+                                Toast.makeText(activity, "25 Coins Added!$gameCoin", Toast.LENGTH_SHORT)
+                                    .show()
+                                counter++
+                                checkin++
+                                databaseReference.child(sessionUser).child("checkin").setValue(checkin)
+                                databaseReference.child(sessionUser).child("gameCoin").setValue(gameCoin)
+
+                                //Show Pop To Notify User
+                                builder = AlertDialog.Builder(activity!!)
+                                builder.setTitle("Congratulations!")
+                                    .setMessage("You have checked in for 7 days")
+                                    .setCancelable(true)
+                                    .setPositiveButton("Ok") { dialogInterface, it ->
+                                        bindingCheckIn.progressBarCheckIn.progress = 0
+                                        bindingCheckIn.imageViewDay1.setImageResource((R.drawable.check_in_progress))
+                                        bindingCheckIn.imageViewDay2.setImageResource((R.drawable.check_in_progress))
+                                        bindingCheckIn.imageViewDay3.setImageResource((R.drawable.check_in_progress))
+                                        bindingCheckIn.imageViewDay4.setImageResource((R.drawable.check_in_progress))
+                                        bindingCheckIn.imageViewDay5.setImageResource((R.drawable.check_in_progress))
+                                        bindingCheckIn.imageViewDay6.setImageResource((R.drawable.check_in_progress))
+                                        bindingCheckIn.imageViewDay7.setImageResource((R.drawable.check_in_progress))
+                                        dialogInterface.cancel()
+                                    }
+                                    .show()
+                            }
+                        }
+                    }else{
+                        Toast.makeText(activity, "Already Checked In Today!\nPlease Try Again Tomorrow!", Toast.LENGTH_SHORT).show()
+                    }
+
+
+                }else {
+                    Toast.makeText(activity, "User Doesn't Exists", Toast.LENGTH_SHORT).show()
+                }
+            }.addOnFailureListener{
+                Toast.makeText(activity, "Failed to Retrieve User", Toast.LENGTH_SHORT).show()
+            }
         }
+
+
         //TODO: setAlarm function and create variables to be stored as data set
 
         //Nav to History Page
@@ -171,9 +224,7 @@ class CheckIn: Fragment() {
     //Function to Constantly Update Counter without Delay
     private val updateCounter = object : Runnable {
         override fun run() {
-//            counterTextView.text = gameCoinBal.toString()
-            getSess()
-            displayinfo()
+            counterTextView.text = gameCoin.toString()
             handler.postDelayed(this, 0) // run instantly
         }
     }
@@ -201,27 +252,6 @@ class CheckIn: Fragment() {
 //        }
 //    }
 
-    private fun displayinfo(){
-        getData(user)
-    }
-
-    private fun getData(username: String) {
-        //get coin
-        databaseReference.child(username).child("gameCoin").get().addOnSuccessListener {
-            val coinBal = it.getValue(Int::class.java).toString()
-            bindingCheckIn.textViewRegBalance.text = coinBal}
-    }
-
-    private fun getSess(){
-        val preferences = requireContext().getSharedPreferences("sess_store", Context.MODE_PRIVATE)
-        val sess_username = preferences.getString("username", "")
-        if (sess_username != ""){
-            user = sess_username!!
-        } else {
-            Toast.makeText(activity,"failed to retrieve username", Toast.LENGTH_SHORT).show()
-        }
-    }
-
     private fun replaceFragment(fragment : Fragment){
 
         val fragmentManager = activity?.supportFragmentManager
@@ -232,5 +262,6 @@ class CheckIn: Fragment() {
         fragmentTransaction?.replace(R.id.frameLayout, fragment)
         fragmentTransaction?.commit()
     }
+
 
 }
--- app/src/main/java/my/edu/tarc/assignment/MainActivity.kt
+++ app/src/main/java/my/edu/tarc/assignment/MainActivity.kt
@@ -1,15 +1,19 @@
 package my.edu.tarc.assignment
 
 import android.content.Intent
+import android.content.SharedPreferences
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import my.edu.tarc.assignment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
+    private lateinit var sf: SharedPreferences
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +21,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         // This is what users see when they first use our app (in this case: CheckIn Fragment will be shown first)
         replaceFragment(CheckIn())
+        sf = getSharedPreferences("sess_store", MODE_PRIVATE)
         binding.bottomNavigationView.setOnItemSelectedListener {
+            Log.i("Session", sf.getString("username", null).toString())
             when(it.itemId) {
                 R.id.checkin -> replaceFragment(CheckIn())
                 R.id.quiz -> replaceFragment(Quiz())
@@ -25,7 +31,7 @@ class MainActivity : AppCompatActivity() {
                 R.id.tree -> replaceFragment(Tree())
                 R.id.profile -> replaceFragment(Profile())
 
-            // This is for testing commit and push purposes
+                // This is for testing commit and push purposes
             }
 
             true
@@ -53,5 +59,10 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this, LoginActivity::class.java)
         startActivity(intent)
         finish()
+    }
+
+    fun getSessionUser(): String {
+        sf = getSharedPreferences("sess_store", MODE_PRIVATE)
+        return sf.getString("username", null).toString()
     }
 }
